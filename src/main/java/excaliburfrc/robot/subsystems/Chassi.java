fa815bc2c5d55e2dc7a5541fd75b2341dac469a5
Starlight220 <yotamshlomi@gmail.com>
2020-11-11 22:13:55 +0200

    init

---
+++ src/main/java/excaliburfrc/robot/subsystems/Chassi.java
@@ -0,0 +1,79 @@
+package excaliburfrc.robot.subsystems;
+
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import static com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushless;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.controller.RamseteController;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static excaliburfrc.robot.subsystems.Ports.ChassiPorts.*;
+
+public class Chassi extends SubsystemBase {
+    private CANSparkMax leftLeader;
+    private CANSparkMax leftFollower;
+    private CANSparkMax rightLeader;
+    private CANSparkMax rightFollower;
+    private Encoder leftEncoder;
+    private Encoder rightEncoder;
+    private AHRS gyro;
+
+    private DifferentialDrive drive;
+    private DifferentialDriveOdometry odometry;
+
+    public Chassi() {
+        leftLeader = new CANSparkMax(LEFT_LEADER, kBrushless);
+        leftFollower = new CANSparkMax(LEFT_FOLLOWER, kBrushless);
+        leftFollower.follow(leftLeader);
+
+        rightLeader = new CANSparkMax(RIGHT_LEADER, kBrushless);
+        rightFollower = new CANSparkMax(RIGHT_FOLLOWER, kBrushless);
+        rightFollower.follow(rightLeader);
+
+        leftEncoder = new Encoder(LEFT_ENCODER_A, LEFT_ENCODER_B, false);
+        leftEncoder.setDistancePerPulse(GEARING);
+        rightEncoder = new Encoder(RIGHT_ENCODER_A, RIGHT_ENCODER_B, true);
+        rightEncoder.setDistancePerPulse(GEARING);
+
+        gyro = new AHRS(SPI.Port.kMXP);
+
+        drive = new DifferentialDrive(leftLeader, rightLeader);
+        drive.setSafetyEnabled(false);
+
+        odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(0.0));
+    }
+
+    @Override
+    public void periodic() {
+        odometry.update(Rotation2d.fromDegrees(gyro.getAngle()), leftEncoder.get(), rightEncoder.get());
+    }
+
+    public Command buildRamseteCommand(Trajectory trajectory) {
+        return new RamseteCommand(
+                                trajectory, 
+                                odometry::getPoseMeters, 
+                                new RamseteController(),
+                                new SimpleMotorFeedforward(kV, kS, kA),
+                                new DifferentialDriveKinematics(TRACK_WIDTH),
+                                ()->new DifferentialDriveWheelSpeeds(leftEncoder.getRate(), rightEncoder.getRate()),
+                                new PIDController(0, 0, 0),
+                                new PIDController(0, 0, 0),
+                                (left, right)->drive.tankDrive(RobotController.getBatteryVoltage() * left, RobotController.getBatteryVoltage() * right),
+                                // (double left, double right)->drive.set, 
+                                this)
+                                .andThen(drive::stopMotor, this);
+    }
+
+}
---
+++ update-vendors.ps1
@@ -0,0 +1,4 @@
+./gradlew vendordep --url=https://www.kauailabs.com/dist/frc/2020/navx_frc.json
+./gradlew vendordep --url=`$FRCLOCAL/WPILibNewCommands.json
+./gradlew vendordep --url=http://www.revrobotics.com/content/sw/max/sdk/REVRobotics.json
+./gradlew vendordep --url=http://devsite.ctr-electronics.com/maven/release/com/ctre/phoenix/Phoenix-latest.json
