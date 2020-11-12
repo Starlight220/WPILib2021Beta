723e6bfa398370f2213d3a1b11cd3953231c5d4b
Starlight220 <yotamshlomi@gmail.com>
2020-11-12 19:05:08 +0200

    get JUnit5 to work

--- src/test/java/excaliburfrc/robot/TestPath.java
+++ src/test/java/excaliburfrc/robot/TestPath.java
@@ -1,38 +1,33 @@
 package excaliburfrc.robot;
 
-import static org.junit.Assert.fail;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.wpi.first.hal.HAL;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.simulation.DriverStationSim;
+import edu.wpi.first.wpilibj.simulation.SimHooks;
 import edu.wpi.first.wpilibj.trajectory.Trajectory.State;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import java.io.IOException;
-import org.junit.BeforeClass;
-import org.junit.Test;
 
 public class TestPath {
   Robot robot;
   State[] states;
 
-  @BeforeClass
-  public void setup() {
-    robot = new Robot();
-    try {
-      states =
-          new ObjectMapper()
-              .readValue(
-                  Filesystem.getDeployDirectory().toPath().resolve("forward.wpilib.json").toFile(),
-                  State[].class);
-    } catch (IOException e) {
-      e.printStackTrace();
-      fail(e.getMessage());
-    }
-    robot.startCompetition();
-  }
-
   @Test
-  public void testPathFollowing() {
+  public void testPathFollowing() throws IOException {
+    if (!HAL.initialize(500, 0)) throw new IllegalStateException("Was unable to initialize");
+    robot = new Robot();
+    states = new ObjectMapper().readValue(
+                            Filesystem.getDeployDirectory().toPath().resolve("forward.wpilib.json").toFile(),
+                            State[].class);
+
+    Thread robotThread = new Thread(() -> robot.startCompetition());
+    robotThread.start();
+    SimHooks.pauseTiming();
     DriverStationSim.setAutonomous(true);
     DriverStationSim.setEnabled(true);
+
   }
 }
