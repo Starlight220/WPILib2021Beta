4001d08f74e3b7d8cecaefb4e859fe756846c544
Starlight220 <yotamshlomi@gmail.com>
2020-11-12 14:45:49 +0200

    add spotless

--- src/main/java/excaliburfrc/robot/subsystems/Chassi.java
+++ src/main/java/excaliburfrc/robot/subsystems/Chassi.java
@@ -1,13 +1,14 @@
 package excaliburfrc.robot.subsystems;
 
+import static com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushless;
+import static excaliburfrc.robot.subsystems.Ports.ChassiPorts.*;
+
 import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkMax;
-import static com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushless;
-
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.controller.RamseteController;
 import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -19,61 +20,62 @@ import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import static excaliburfrc.robot.subsystems.Ports.ChassiPorts.*;
 
 public class Chassi extends SubsystemBase {
-    private CANSparkMax leftLeader;
-    private CANSparkMax leftFollower;
-    private CANSparkMax rightLeader;
-    private CANSparkMax rightFollower;
-    private Encoder leftEncoder;
-    private Encoder rightEncoder;
-    private AHRS gyro;
+  private CANSparkMax leftLeader;
+  private CANSparkMax leftFollower;
+  private CANSparkMax rightLeader;
+  private CANSparkMax rightFollower;
+  private Encoder leftEncoder;
+  private Encoder rightEncoder;
+  private AHRS gyro;
 
-    private DifferentialDrive drive;
-    private DifferentialDriveOdometry odometry;
+  private DifferentialDrive drive;
+  private DifferentialDriveOdometry odometry;
 
-    public Chassi() {
-        leftLeader = new CANSparkMax(LEFT_LEADER, kBrushless);
-        leftFollower = new CANSparkMax(LEFT_FOLLOWER, kBrushless);
-        leftFollower.follow(leftLeader);
+  public Chassi() {
+    leftLeader = new CANSparkMax(LEFT_LEADER, kBrushless);
+    leftFollower = new CANSparkMax(LEFT_FOLLOWER, kBrushless);
+    leftFollower.follow(leftLeader);
 
-        rightLeader = new CANSparkMax(RIGHT_LEADER, kBrushless);
-        rightFollower = new CANSparkMax(RIGHT_FOLLOWER, kBrushless);
-        rightFollower.follow(rightLeader);
+    rightLeader = new CANSparkMax(RIGHT_LEADER, kBrushless);
+    rightFollower = new CANSparkMax(RIGHT_FOLLOWER, kBrushless);
+    rightFollower.follow(rightLeader);
 
-        leftEncoder = new Encoder(LEFT_ENCODER_A, LEFT_ENCODER_B, false);
-        leftEncoder.setDistancePerPulse(GEARING);
-        rightEncoder = new Encoder(RIGHT_ENCODER_A, RIGHT_ENCODER_B, true);
-        rightEncoder.setDistancePerPulse(GEARING);
+    leftEncoder = new Encoder(LEFT_ENCODER_A, LEFT_ENCODER_B, false);
+    leftEncoder.setDistancePerPulse(GEARING);
+    rightEncoder = new Encoder(RIGHT_ENCODER_A, RIGHT_ENCODER_B, true);
+    rightEncoder.setDistancePerPulse(GEARING);
 
-        gyro = new AHRS(SPI.Port.kMXP);
+    gyro = new AHRS(SPI.Port.kMXP);
 
-        drive = new DifferentialDrive(leftLeader, rightLeader);
-        drive.setSafetyEnabled(false);
+    drive = new DifferentialDrive(leftLeader, rightLeader);
+    drive.setSafetyEnabled(false);
 
-        odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(0.0));
-    }
+    odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(0.0));
+  }
 
-    @Override
-    public void periodic() {
-        odometry.update(Rotation2d.fromDegrees(gyro.getAngle()), leftEncoder.get(), rightEncoder.get());
-    }
+  @Override
+  public void periodic() {
+    odometry.update(Rotation2d.fromDegrees(gyro.getAngle()), leftEncoder.get(), rightEncoder.get());
+  }
 
-    public Command buildRamseteCommand(Trajectory trajectory) {
-        return new RamseteCommand(
-                                trajectory, 
-                                odometry::getPoseMeters, 
-                                new RamseteController(),
-                                new SimpleMotorFeedforward(kV, kS, kA),
-                                new DifferentialDriveKinematics(TRACK_WIDTH),
-                                ()->new DifferentialDriveWheelSpeeds(leftEncoder.getRate(), rightEncoder.getRate()),
-                                new PIDController(0, 0, 0),
-                                new PIDController(0, 0, 0),
-                                (left, right)->drive.tankDrive(RobotController.getBatteryVoltage() * left, RobotController.getBatteryVoltage() * right),
-                                // (double left, double right)->drive.set, 
-                                this)
-                                .andThen(drive::stopMotor, this);
-    }
-
+  public Command buildRamseteCommand(Trajectory trajectory) {
+    return new RamseteCommand(
+            trajectory,
+            odometry::getPoseMeters,
+            new RamseteController(),
+            new SimpleMotorFeedforward(kV, kS, kA),
+            new DifferentialDriveKinematics(TRACK_WIDTH),
+            () -> new DifferentialDriveWheelSpeeds(leftEncoder.getRate(), rightEncoder.getRate()),
+            new PIDController(0, 0, 0),
+            new PIDController(0, 0, 0),
+            (left, right) ->
+                drive.tankDrive(
+                    RobotController.getBatteryVoltage() * left,
+                    RobotController.getBatteryVoltage() * right),
+            // (double left, double right)->drive.set,
+            this)
+        .andThen(drive::stopMotor, this);
+  }
 }
--- src/main/java/excaliburfrc/robot/subsystems/Ports.java
+++ src/main/java/excaliburfrc/robot/subsystems/Ports.java
@@ -1,26 +1,15 @@
 package excaliburfrc.robot.subsystems;
 
 class Ports {
-    static class ChassiPorts {
-        static final int LEFT_LEADER = 10,
-                                RIGHT_LEADER = 11,
-                                LEFT_FOLLOWER = 12,
-                                RIGHT_FOLLOWER = 13,
-                                
-                                LEFT_ENCODER_A = 0,
-                                RIGHT_ENCODER_A = 1,
-                                LEFT_ENCODER_B = 2,
-                                RIGHT_ENCODER_B = 3
-                                
-                                ;
-
-        static final double GEARING = 1.0,
-                            TRACK_WIDTH = 0.6,
-                            kV = 0.1,
-                            kS = 0.1,
-                            kA = 0.2
-
-
-                            ;
-    }
+  static class ChassiPorts {
+    static final int LEFT_LEADER = 10,
+        RIGHT_LEADER = 11,
+        LEFT_FOLLOWER = 12,
+        RIGHT_FOLLOWER = 13,
+        LEFT_ENCODER_A = 0,
+        RIGHT_ENCODER_A = 1,
+        LEFT_ENCODER_B = 2,
+        RIGHT_ENCODER_B = 3;
+    static final double GEARING = 1.0, TRACK_WIDTH = 0.6, kV = 0.1, kS = 0.1, kA = 0.2;
+  }
 }
